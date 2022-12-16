2d218862230f9947db5e2eb5cc3172f4677a1aba
Bhumesh <bhumesh1719@gmail.com>
2022-12-17 00:26:14 +0530

    modified about

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export const About = () => {
           <h1 className="test">About Me</h1>
         </div>
         <div className="col-sm-9 mt-5">
-          <p className="AB">Hi my name is bhumesh.After my bachleor's degree in 2021 I was doing some freelance job in automation feild.But I always had an eye about web dev as it is fun. So i self thought my self to begin my journey to become a web dev. Im a dev who is looking for a job to showcase my skills and upskill my self.I always focus on the project individually and focus in the result. So that it will be useful my working environment.</p>
+          <p className="AB">Hi my name is bhumesh.After my bachleor's degree in 2021 I was doing some freelance job in automation feild.But I always fascinated about web dev as it is fun. So i self thought my self to begin my journey to become a web dev. Im a dev who is looking for a job to showcase my skills and upskill my self.I always focus on the project individually and focus in the result. So that it will be useful my working environment.</p>
         </div>
       </div>
     </div>
